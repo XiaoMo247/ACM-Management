114baedf58cf5724898eb24b477eafb612b4724a
XiaoMo247 <475898752@qq.com>
2025-09-08 13:12:00 +0800

    init vue

--- fronted-ACMM/src/App.vue
+++ fronted-ACMM/src/App.vue
@@ -1,47 +1,23 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>OK</h1>
+    <input type="checkbox" v-model="checked" />
+    <h1 v-if="checked">Checkbox is checked.</h1>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style>
+  h1 {
+    color: red;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
+
+<script>
+  export default {
+    data() {
+      return {
+        checked: false
+      }
+    }
+  }
+</script>
